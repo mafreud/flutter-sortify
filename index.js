2e23c79d221d7bee6ca6ad9edd7e3a6031ea6ae1
Masahiro Aoki <rafe.kun@gmail.com>
2022-02-06 22:48:13 +0900

    update

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ async function format() {
   let myOutput = '';
 let myError = '';
 
-const workingDirectory = path.resolve(process.env.GITHUB_WORKSPACE, core.getInput('working-directory'))
-const options = { cwd: workingDirectory };
+const options = {};
 options.listeners = {
   stdout: (data) => {
     myOutput += data.toString();
@@ -27,16 +26,16 @@ options.listeners = {
     myError += data.toString();
   }
 };
+options.cwd = './lib';
+ await exec.exec('node',['hello.js'],options);
 
-  await exec.exec('flutter analyze');
-  await exec.exec('pub run import_sorter:main -e');
-  await exec.exec('git add .');
-  await exec.exec('git config --global user.email \'freud427@gmail.com\'');
-  await exec.exec('git config --global user.name \'flutter-sortify\'');
-  await exec.exec('git commit -m \'Sortify!\'',options);
-  console.log(myOutput);
-  console.log(myError);
-  await exec.exec(`git push origin ${process.env.GITHUB_REF_NAME}`);
+  // await exec.exec('flutter analyze');
+  // await exec.exec('pub run import_sorter:main -e');
+  // await exec.exec('git add .');
+  // await exec.exec('git config --global user.email \'freud427@gmail.com\'');
+  // await exec.exec('git config --global user.name \'flutter-sortify\'');
+  // await exec.exec('git commit -m \'Sortify!\'');
+  // await exec.exec(`git push origin ${process.env.GITHUB_REF_NAME}`);
   return
 }
 
