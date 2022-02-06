9deee52e6998a06d4d34798d95f7684be162b83a
Masahiro Aoki <rafe.kun@gmail.com>
2022-02-06 20:50:20 +0900

    update

--- index.js
+++ index.js
@@ -12,13 +12,29 @@ async function run() {
 }
 
 async function format() {
-  await exec.exec('flutter analyze');
-  const result = await exec.exec('pub run import_sorter:main -e');
-  console.log(`result: ${result}`);
+
+  let myOutput = '';
+let myError = '';
+
+const options = {};
+options.listeners = {
+  stdout: (data) => {
+    myOutput += data.toString();
+  },
+  stderr: (data) => {
+    myError += data.toString();
+  }
+};
+options.cwd = './lib';
+
+  await exec.exec('flutter analyze', options);
+  console.log(myOutput);
+  console.log(myError);
+  await exec.exec('pub run import_sorter:main -e');
   await exec.exec('git add .');
   await exec.exec('git config --global user.email \'freud427@gmail.com\'');
   await exec.exec('git config --global user.name \'flutter-sortify\'');
-  await exec.exec('git commit -m \'Sortify imports!\'');
+  await exec.exec('git commit -m \'Sortify!\'');
   await exec.exec(`git push origin ${process.env.GITHUB_REF_NAME}`);
   return
 }
