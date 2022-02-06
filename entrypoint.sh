ef56e9d8d290cc968985514718d28dc93e2acac5
Masahiro Aoki <rafe.kun@gmail.com>
2022-02-06 10:36:43 +0900

    update

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-echo "Hello $1, I'm flutter sortify."
-# chmod +rwx /github/home/
-echo "running flutter pub get"
-sudo flutter pub get
-echo "sorting flutter imports"
-sudo flutter pub run import_sorter:main -e
