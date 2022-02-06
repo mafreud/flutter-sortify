fa22d789016ad8647c120d6b4a7e589de63895b2
Masahiro Aoki <rafe.kun@gmail.com>
2022-02-06 15:21:55 +0900

    update

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const exec = require('@actions/exec');
+const fs = require('fs');
+const path = require('path');
 
 // 1. async - awaitを使えるようにする
+// 2. Cultyaでも動くか確認
+// 3. flutter analyzeを実行
 
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  });
+async function run() {
+  try {
+    const workingDirectory = path.resolve(process.env.GITHUB_WORKSPACE, core.getInput('working-directory'))
+    const formatWarningCount = await format(workingDirectory);
+    console.log(formatWarningCount);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-async function asyncCall() {
-  console.log('calling');
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-  // expected output: "resolved"
+async function format(workingDirectory) {
+  let output = '';
+
+  const options = { cwd: workingDirectory };
+  options.listeners = {
+    stdout: (data) => {
+      output += data.toString();
+    },
+    stderr: (data) => {
+      output += data.toString();
+    }
+  };
+
+  const args = ['format', '--output=none'];
+  const lineLength = core.getInput('line-length');
+
+  if (lineLength) {
+    args.push('--line-length');
+    args.push(lineLength);
+  }
+
+  args.push('.');
+
+  await exec.exec('dart', args, options);
+  
+  let warningCount = 0;
+  const lines = output.trim().split(/\r?\n/);
+
+  for (const line of lines) {
+    if (!line.endsWith('.dart')) continue;
+    const file = line.substring(8); // Remove the "Changed " prefix
+
+    console.log(`::warning file=${file}::Invalid format. For more details, see https://dart.dev/guides/language/effective-dart/style#formatting`);
+    warningCount++;
+  }
+
+  return warningCount;
 }
 
-asyncCall();
+run();
