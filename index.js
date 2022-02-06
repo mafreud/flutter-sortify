92b94cf50763523d02d00a6d97c21320e8e372d1
Masahiro Aoki <rafe.kun@gmail.com>
2022-02-06 15:09:31 +0900

    update

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const path = require('path');
-const exec = require('@actions/exec');
-const fs = require('fs');
 
-async function run() {
-    try {
-        const workingDirectory = path.resolve(process.env.GITHUB_WORKSPACE, core.getInput('working-directory'));
-        console.log(workingDirectory);
-    }
-    catch (error) {
-      core.setFailed(error.message);
-    }
-  }
-run();
-
-
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
